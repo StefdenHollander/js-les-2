a741de2ca5d1369bf1badb5b0e0a3944fa14c55b
S den Hollander <302045198@student.rocmondriaan.nl>
2023-09-01 12:45:04 +0200

    opdrachten bijgewerkt.

---
+++ main.js
@@ -0,0 +1,39 @@
+let a = 6
+let b = 8
+
+console.log("bij elkaar",a+b );
+console.log("aftrekken",a-b);
+console.log("keer elkaar",a*b);
+console.log("gedeeld door",a/b);
+console.log("aftrekken",b-a);
+
+let x = 4
+let y = 2
+
+console.log("bij elkaar",x+y );
+console.log("aftrekken",x-y);
+console.log("keer elkaar",x*y);
+console.log("gedeeld door",x/y);
+console.log("aftrekken",y-x);
+
+let salary = 2000
+let raise = 0.05
+
+console.log("keer elkaar",salary*raise);
+
+
+x = 4;
+y = 0;
+
+console.log("delen door",x/y);
+
+let huis = 400000
+let inflatie = 0.025
+
+console.log("inflatie prijs extra",huis*inflatie);
+
+let jas = 100
+let btw = 1.21
+
+console.log ("extra btw",jas*btw);
+
